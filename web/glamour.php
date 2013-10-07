2f45649533c79760929f1f0792581a4f8a39fa89
IvanekPoul <ivanek.poul@gmail.com>
2013-10-08 11:10:59 +1300

    uprava top-menu v templates <ul> a  <li>

--- web/glamour.php
+++ web/glamour.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
        <meta name="copyright" content="Copyright (c) 2013 Ivan Poul"/>
    <meta name="keywords" content="Pakuranga, Manukau, Auckland, professional photographer, photo atelier, photo studio, wedding, party, birthday, glamour, nude, act, color, black, kid, child, children, adult, glamour, art, Auckland, North Island, New Zealand"/>
