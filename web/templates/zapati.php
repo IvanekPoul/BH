4b1c1e1c334e172bc64e7c17321e0e0df752047e
IvanekPoul <ivanek.poul@gmail.com>
2013-10-09 16:13:14 +1300

    css

--- web/templates/zapati.php
+++ web/templates/zapati.php
@@ -2,6 +2,6 @@
 <span class="grey">
 
 	Katerina Dvorakova & Ivan Poul © 2013
- <span>  <a href="./map.php">Page Map</a>    &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/bohemiaphotographycom" target="blank"><img src="./img/fb.jpg" height="40" alt="Like it!"/></a>
+ <span>  <a href="./map.php">Page Map</a>    &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/bohemiaphotographycom" target="blank"><img src="./img/fb.jpg" height="30" alt="Like it!"/></a>
  
 <!-- FOOTER END -->
