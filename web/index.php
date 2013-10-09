48fb9d8b3ee67f2924349836e438d4d637a4e4f1
IvanekPoul <ivanek.poul@gmail.com>
2013-10-09 17:09:09 +1300

    css

--- web/index.php
+++ web/index.php
@@ -157,7 +157,7 @@ function turnOff(image8) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  js.src = "//connect.facebook.net/cs_CZ/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
@@ -170,17 +170,9 @@ function turnOff(image8) {
        <div>
         <?php include_once('templates/top_menu.php');?>
        </div>
-       <div id="fbico">
-       
+       <div class="fb-like" data-href="https://www.facebook.com/bohemiaphotographycom" data-width="The pixel width of the plugin" data-height="30" data-colorscheme="dark" data-layout="standard" data-action="like" data-show-faces="false" data-send="true">
        </div>
-       <div id="fb">
-       <img src="./img/fb.jpg" height="20" alt="FaceBook"/>
-       <data-href="https://www.facebook.com/bohemiaphotographycom" data-width="The pixel width of the plugin" data-height="20" data-colorscheme="dark" data-layout="button_count" data-action="like" data-show-faces="true" data-send="true" class="fb-like" >
-       </div>
-          <div class="fb-like" data-href="https://www.facebook.com/bohemiaphotographycom" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="dark" data-layout="standard" data-action="like" data-show-faces="true" data-send="false"></div>
-    <div class="fb-like" data-href="https://www.facebook.com/bohemiaphotographycom" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="dark" data-layout="button_count" data-action="recommend" data-show-faces="true" data-send="false"></div>
-    </div>
-   <br/>                                                     
+     <br/>                                                     
  <div id="main">
       <div id="mright">
    <!-- PAGE CONTENT START -->
