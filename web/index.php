b710a38dd615f6ce188d7872d9b54b825cd7e226
IvanekPoul <ivanek.poul@gmail.com>
2013-10-09 13:22:37 +1300

    try facebook like v index.php, nenagravam na FTP

--- web/index.php
+++ web/index.php
@@ -151,11 +151,23 @@ function turnOff(image8) {
 }
 -->
 </script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/cs_CZ/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
   </head>
   <body>
      <div id="top">
        <div id="logo">
-        <a href="/index.php"><img src="images/logo.jpg" alt="BohemiaPhotography.com" border="0" /></a>            
+        <a href="/index.php"><img src="images/logo.jpg" alt="BohemiaPhotography.com" border="0" /></a>  
+        <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="standard" data-action="like" data-show-faces="true" data-send="false"></div>          
       </div><!-- logo -->
       <div>
         <?php include_once('templates/top_menu.php');?>
