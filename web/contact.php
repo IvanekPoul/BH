a4323f4c8ad864ebf7404408d15dc82eb3ebc121
IvanekPoul <ivanek.poul@gmail.com>
2013-10-11 15:40:07 +1300

    text in contact

--- web/contact.php
+++ web/contact.php
@@ -7,17 +7,22 @@
  <h1 class="nadpis"><span>Contact</span></h1>
  <br/>
   <p align="justify">
-What to do?
-The first step is to contact a photographer via mobile or e-mail and book a consultation in your convenience. It is the most easy way to reach fantastic photos. Having a chat with a professional means discuss your intentions anr to find a perfect solution for you. 
-Not without sucess is our motto: "Let us captuje your unique soul". We will explain you how to start the photo sesion, what do wear on, what style of make-up style will be perfect for the photosesion fiting with you special ocassion.
+What to do?  
 <br/>
-We would like to give you a piece of advice what to wear on to have perfect photos. Book a consultation, it's free! 
+The first step is to <strong>contact a photographer</strong> via mobile or e-mail and book a consultation in your convenience. 
+It is the most easy way to reach fantastic photos. Having a chat with a professional means discuss your intentions to find a perfect solution for you. 
+Not without sucess is our motto: "Let us capture your unique soul". Durring the consultation we will explain you how to start the photo sesion, what do wear on, what style of make-up will be perfect for the photo-session fiting with you special ocassion.
+<br/>
+We would like to give you a piece of advice and will find some information about you. Book a consultation, it's free! 
 Moreover we could easily arrange a price to your conveniece.
 <br/>
 We offer remedial packages in different prices e.g. <strong>Wedding Photography</strong>: "Kiss session", "Middle" = Kiss session + Ceremony, "Big" Kiss session + Ceremony + Party
 <br/>
 Do not hesitate to contact us with your special wish..., tell us your dreams on photo-session.
+<br/>
+<strong>Contact us for photo-session or event pricing!</strong>
    </p>
+   <br/>
 <address>
   <strong>Ivan Poul & Katerina Dvorakova</strong><br />
 
