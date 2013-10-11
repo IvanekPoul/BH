766db11f1784ac2520124c79e1071b13e9335f3b
IvanekPoul <ivanek.poul@gmail.com>
2013-10-11 16:06:58 +1300

    text in contact

--- web/contact.php
+++ web/contact.php
@@ -13,26 +13,25 @@ The first step is to <strong>contact a photographer</strong> via mobile or e-mai
 </p>
 <p>
 <h2>Consultation</h2>
-<br/>
+
 It is the easiest way how to reach fantastic photos. Having a chat with a professional it means discuss your intentions to find a perfect solution for you. 
 Not without success is our motto: <i>"Let us capture your unique soul"</i>. During the consultation we will explain you how to start the photo session, what 
 to wear on, what style of make-up will be perfect for the photo-session fitting with you special occasion.
 </p>
-<br/>
 <p>
 We would like to give you a piece of advice moreover we will get some information about you. Book a consultation now, it's free! 
 During consultation we could easily arrange a price for the photo-session to your convenience.
 </p>
-<br/>
 <p>
 <h2>What we offer?</h2>
-<br/>
-We offer remedial packages in different prices e.g. <strong>Wedding Photography</strong>: <i>"Kiss session"</i>, <i>"Middle"</i>package = Kiss session + Ceremony, <i>"Big"</i> package = Kiss session + Ceremony + Party (approx. 8 hrs.)
-<br/>
 <h3><a href="./adults.php" target="blank">Portrait</a> - <a href="./wedding.php" target="blank">Wedding</a> 
 - <a href="./family.php" target="blank"> Family & Kids</a> - <a href="./glamour.php" target="blank">Galmour</a> - <a href="./event.php" target="blank"> Events </a>
 - <a href="./nature.php" target="blank"> Nature & Procut Photos</a>
 </h3>
+<br/>
+We offer remedial packages in different prices e.g. <strong>Wedding Photography</strong>: <i>"Kiss session"</i>, <i>"Middle"</i>package = Kiss session + Ceremony, <i>"Big"</i> package = Kiss session + Ceremony + Party (approx. 8 hrs.)
+<br/>
+Our photes goes throught basic or advanced digital retouch, learn more about a <a href="./work.php" target="blank">post-processing</a>.
 </p>
 <p>         
 Do not hesitate to contact us with your special wish..., tell us your dreams on photo-session.
