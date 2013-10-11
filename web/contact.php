2a89b654b0d5c738a57bcdd7b4f3286170433889
IvanekPoul <ivanek.poul@gmail.com>
2013-10-11 15:35:26 +1300

    change in contact (just text)

--- web/contact.php
+++ web/contact.php
@@ -7,10 +7,16 @@
  <h1 class="nadpis"><span>Contact</span></h1>
  <br/>
   <p align="justify">
-Book a consultation to have a chat with a professional how to start the photo sesion in your convenience. We would like to give you a piece of advice what to wear on to have perfect photos. It's free!
+What to do?
+The first step is to contact a photographer via mobile or e-mail and book a consultation in your convenience. It is the most easy way to reach fantastic photos. Having a chat with a professional means discuss your intentions anr to find a perfect solution for you. 
+Not without sucess is our motto: "Let us captuje your unique soul". We will explain you how to start the photo sesion, what do wear on, what style of make-up style will be perfect for the photosesion fiting with you special ocassion.
 <br/>
+We would like to give you a piece of advice what to wear on to have perfect photos. Book a consultation, it's free! 
+Moreover we could easily arrange a price to your conveniece.
 <br/>
-Do not hesitate to contact us with your special wish...
+We offer remedial packages in different prices e.g. <strong>Wedding Photography</strong>: "Kiss session", "Middle" = Kiss session + Ceremony, "Big" Kiss session + Ceremony + Party
+<br/>
+Do not hesitate to contact us with your special wish..., tell us your dreams on photo-session.
    </p>
 <address>
   <strong>Ivan Poul & Katerina Dvorakova</strong><br />
@@ -19,7 +25,7 @@ Do not hesitate to contact us with your special wish...
   Pakuranga<br />
   Auckland 2010, New Zealand <br /><br />
 
-  <label>Tel.:</label> +64 22 305 3192<br />
+  <label>Mobile:</label> +64 22 305 3192<br />
   <label>E-mail.:</label> info@bohemiaphotography.com<br />
                   </address>
             <br />
