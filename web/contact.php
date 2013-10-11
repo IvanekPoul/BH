9c8353a05e0d126099a4b22c67ab381e4ebe1430
IvanekPoul <ivanek.poul@gmail.com>
2013-10-11 16:15:23 +1300

    text

--- web/contact.php
+++ web/contact.php
@@ -9,8 +9,7 @@
 <p>
 <h2>What to do?</h2>  
 The first step is to <strong>contact a photographer</strong> via mobile or e-mail and book a consultation in your convenience and/or just to ask for a price.
-</p>
-<p>
+<br/>
 <h2>Consultation</h2>
 It is the easiest way how to reach fantastic photos. Having a chat with a professional it means discuss your intentions to find a perfect solution for you. 
 Not without success is our motto: <i>"Let us capture your unique soul"</i>. During the consultation we will explain you how to start the photo session, what 
@@ -18,8 +17,7 @@ to wear on, what style of make-up will be perfect for the photo-session fitting 
 <br/>
 We would like to give you a piece of advice moreover we will get some information about you. Book a consultation now, it's free! 
 During consultation we could easily arrange a price for the photo-session to your convenience.
-</p>
-<p>
+<br/>
 <h2>What we offer?</h2>
 <h4><a href="./adults.php" target="blank">Portrait</a> - <a href="./wedding.php" target="blank">Wedding</a> 
 - <a href="./family.php" target="blank"> Family & Kids</a> - <a href="./glamour.php" target="blank">Galmour</a> - <a href="./event.php" target="blank"> Events </a>
@@ -31,11 +29,10 @@ We offer remedial packages in different prices e.g. <strong>Wedding Photography<
 Our photes goes throught basic or advanced digital retouch, learn more about a <a href="./work.php" target="blank">post-processing</a>.
 <br/>        
 Do not hesitate to contact us with your special wish..., tell us your dreams on photo-session.
-</p>
-<p>
+<br/>
 <h2>Pricing</h2>
 <strong>Contact us for photo-session/event pricing now! We are expecting your phone-call or e-mail.</strong>
-   </p>
+  
    <br/>
 <address>
   <strong>Ivan Poul & Katerina Dvorakova</strong><br />
