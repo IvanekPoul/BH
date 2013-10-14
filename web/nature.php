abb9f89dfd22b04d27b2fccb76aaa2d6a347c7c2
IvanekPoul <ivanek.poul@gmail.com>
2013-10-14 16:07:21 +1300

    add 1 photo to nature

--- web/nature.php
+++ web/nature.php
@@ -13,10 +13,13 @@
 
 
  </a>
- <a href="/images/country/201310091.jpg" title="Beach" class="fancybox" rel="group">
+ <a href="/images/country/201310091.jpg" title="Waihi Beach" class="fancybox" rel="group">
   <img src="/images/country/201310091a.jpg" height="130" alt="Beach" class="img_link" /></a> 
-   <a href="/images/country/2013100911.jpg" title="Beach" class="fancybox" rel="group">
+   <a href="/images/country/2013100911.jpg" title="Waihi Beach" class="fancybox" rel="group">
   <img src="/images/country/2013100911a.jpg" height="130" alt="Beach" class="img_link" /></a> 
+  <a href="/images/country/20131014.jpg" title="Onehung Wharf" class="fancybox" rel="group">
+  <img src="/images/country/20131014a.jpg" height="130" alt="Beach" class="img_link" /></a> 
+  
 
   <a href="/images/country/c1.jpg" title="Ladakh" class="fancybox" rel="group" >
 
