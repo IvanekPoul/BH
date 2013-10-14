9c7e1287e0f45adaff7db121fc73d1cff555302c
IvanekPoul <ivanek.poul@gmail.com>
2013-10-15 11:07:36 +1300

    uprava favourite

--- web/favourite.php
+++ web/favourite.php
@@ -1,35 +1,34 @@
 <?php $title = "Bohemia Photography"; ?>
 <?php include_once("./templates/html_header.php");?>
 
-    <br>
+    <br/>
 
-    <br>
+    <br/>
 
-    <br>
-
+  
   <p align="left">  
 
     <h2>
 
 <a href="http://www.frantisek.co.nz" target="blank">FRANTISEK</a>, Fashion for New Zealand
+     <br/>
+<a href="http://www.nikon.com" target="blank">NIKON</a> Cameras and lenses, what we use    
+     <br/>
+<a href="http://www.sigmaphoto.com/lenses" target="blank">SIGMA</a> Lenses, what we use
+     <br/>
+     <a href="http://www.aucklandartgallery.com/neighbourhood/events" target="blank"> Auckland ART Galery</a>
 
-   <br>
+    <br/>
 
-    <br>
+    <br/>
 
-    <br>
+    <br/>
 
-    <br>
+    <br/>
 
-    <br>
+    <br/>
 
-    <br>
-
-    <br>
-
-    <br>
-
-    <br>  
+    <br/>  
 
      
 
