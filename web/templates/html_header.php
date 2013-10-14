59d545de490abd16d9f7ee928bf68ccdf3183b31
IvanekPoul <ivanek.poul@gmail.com>
2013-10-15 11:40:15 +1300

    header

--- web/templates/html_header.php
+++ web/templates/html_header.php
@@ -26,6 +26,7 @@
           <?php include_once('templates/top_menu.php');?>
         </div>
         <div id="fb" class="fb-like" data-href="https://www.facebook.com/bohemiaphotographycom" data-width="The pixel width of the plugin" data-height="30" data-colorscheme="dark" data-layout="standard" data-action="like" data-show-faces="false" data-send="true"></div>
+        <div class="clear"></div>
       </div>    
         
       <div id="mright">
