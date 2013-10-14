675241b185ab475728129be93bdb9c95e06b90c9
IvanekPoul <ivanek.poul@gmail.com>
2013-10-15 11:09:34 +1300

    favourite

--- web/favourite.php
+++ web/favourite.php
@@ -2,21 +2,25 @@
 <?php include_once("./templates/html_header.php");?>
 
     <br/>
-
+      <br/>
     <br/>
+      <br/>
+        <br/>
+        
+    
 
   
   <p align="left">  
 
-    <h2>
-
+  <strong>  
 <a href="http://www.frantisek.co.nz" target="blank">FRANTISEK</a>, Fashion for New Zealand
      <br/>
 <a href="http://www.nikon.com" target="blank">NIKON</a> Cameras and lenses, what we use    
      <br/>
 <a href="http://www.sigmaphoto.com/lenses" target="blank">SIGMA</a> Lenses, what we use
      <br/>
-     <a href="http://www.aucklandartgallery.com/neighbourhood/events" target="blank"> Auckland ART Galery</a>
+<a href="http://www.aucklandartgallery.com/neighbourhood/events" target="blank"> Auckland ART Galery</a>
+  </strong>
 
     <br/>
 
@@ -26,15 +30,8 @@
 
     <br/>
 
-    <br/>
 
-    <br/>  
 
-     
-
-     </h2>
-
-  </div>
 
    </p>
 
