cfef6cc0ea2bc06cd014ff62e6e4d3c6f925d54f
IvanekPoul <ivanek.poul@gmail.com>
2013-10-15 15:05:15 +1300

    uprava tagu work.php

--- web/work.php
+++ web/work.php
@@ -1,29 +1,56 @@
-<?php $title = "How do we work?"; ?>
+<?php $title = "How Do We Work?"; ?>
 <?php include_once("./templates/html_header.php");?>
 
 <script type="text/javascript" src="js/work.js"></script>
 
-<h1>How do We Work?</h1>
-<br/>
+<h1>How Do We Work?</h1>
+ <br/>
+<a href="./work.php#work-with-light">Work With Light</a>, 
+<a href="./work.php#client-care">Client Care</a>, 
+<a href="./work.php#post-processing">Post-Processing</a>,
+
 <p>
-All our images go through software post-processing turning you into profesionally looking photo model and making art work 
-from common plain shot.
-        <br/>  <br/>
+We offer to our clients photo-sessions outdoor (park, countryside, town, beach, wharf etc.) and indoor (our photo studio, church, concert hall, exhibition centre etc.).
+For the best studio photography we use 3 professional 600 W studio flash-lights and various backgrounds. 
+Outdoors is commonly used collapsible reflector or non-frontal remote flash.
+In a special occasion we are able mooving our studio equipment into your choosen place (workshop, church...). All our images go through software post-processing turning you into profesionally looking photo model and making art work 
+from common plain shot.                      
+  <br/>
+  <br/>
+<a name="work-with-light">   </a>  
+WORK WITH LIGHT
+  <br/>
+  
+  <br/>
+  <br/>        
+<a name="client-care">   </a>   
+CLIENT CARE
+  <br/>
+  
+  <br/>
+  <br/>        
+<a name="post-processing">   </a> 
+POST-PROCESSING
+  <br/>    
 <strong>Basic</strong> post-processing contains white balance, colour and exposure correction, sharpening, cropping, etc.
-<br/> <br/>
+  <br/> 
+  <br/>
 <strong>Advanced</strong> post-processing is highly recommended and raises your beauty even more. It contains skin blemish and wrinkles retouching, 
 makeup corrections and smoothing, removal of stray hairs, figure shaping, removal of tattoo & piercing if required, clothes touch-ups, 
 background adjustments, lighting corrections, etc.
-<br/>  <br/>
+  <br/> 
+  <br/>
 Here we would like to show, how our photo postproduction works. Try to move your mouse over the picture below to see before and after.
- <br/><br/>
-   <a href="./images/postproduction/2.jpg" onMouseOver="turnOn('image1T')" onMouseOut="turnOff('image1T')" rel="lightbox[g]">
-<img name="image1T" src="images/postproduction/2.jpg" alt="Postproduction" width="400" border="0"
-onmouseover="window.status='Postproduction'; return true" onmouseout="window.status='SSSSSS'; return true" class="img_link" ></a>
+  <br/>
+  <br/>
+<a href="./images/postproduction/2.jpg" onMouseOver="turnOn('image1T')" onMouseOut="turnOff('image1T')" rel="lightbox[g]">
+<img name="image1T" src="images/postproduction/2.jpg" alt="Postproduction" width="300" border="0"
+  onmouseover="window.status='Postproduction'; return true" onmouseout="window.status='SSSSSS'; return true" class="img_link" ></a>
 
 <a href="./images/postproduction/3.jpg" onMouseOver="turnOn('image2T')" onMouseOut="turnOff('image2T')" rel="lightbox[g]">
-<img name="image2T" src="images/postproduction/3.jpg" alt="TRETAMENT" width="400" border="0"
-onmouseover="window.status='Postproduction'; return true" onmouseout="window.status='SSSSSS'; return true" class="img_link" ></a>
+<img name="image2T" src="images/postproduction/3.jpg" alt="TRETAMENT" width="300" border="0"
+  onmouseover="window.status='Postproduction'; return true" onmouseout="window.status='SSSSSS'; return true" class="img_link" ></a>
+</p>
 <div class="separator"></div>
 
 <?php include_once("./templates/html_footer.php");?>
