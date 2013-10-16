004444d54c2c44fde44e37b004df80b047e0415e
IvanekPoul <ivanek.poul@gmail.com>
2013-10-16 20:16:02 +1300

    ...

--- web/templates/left-menu-text.php
+++ web/templates/left-menu-text.php
@@ -15,11 +15,7 @@ Hello, I am Katerina.
 <br/>
 <br/>
 My passion for photos was inherited from my dad and grandpa. I have been enthusiastic about computer photo manipulation since I was a kid, later in connection with 3D virtual creation.
-<br/>
-<br/>
 I am passionate not only about taking shots but also about graphics including post-production (retouching) of your photos, I will help you with choice of clothes and localities for your portrait pictures.
-<br/>
-<br/>
 As a graduated architect, I am focused on details and creative approach, the final product must reflect your both inner and outer beauty and capture your unique personality.
         </p>
 
