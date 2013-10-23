63e6db360fb41a3cdb7bf9d21f3c05ed891d5767
IvanekPoul <ivanek.poul@gmail.com>
2013-10-24 12:01:20 +1300

    ....

--- web/try.php
+++ web/try.php
@@ -1,17 +1,7 @@
 <?php $title = "News"; ?>
 <?php include_once("./templates/html_header.php");?>
 
-<script type="text/javascript">
-    var mygallery=new fadeSlideShow({
-	    wrapperid: "fadeshow1", 
-	    dimensions: [900, 598], 
-	    imagearray: [<?php include("./generate.php");?>],
-	    displaymode: {type:'auto', pause:8000, cycles:0, wraparound:false},
-	    persist: false, //remember last viewed slide and recall within same session?
-	    fadeduration: 2500, //transition duration (milliseconds)
-	    descreveal: "ondemand",
-	    togglerid: ""})
-    </script>
+
 
 
    <h1>Cook Islanders</h1>
