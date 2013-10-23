22b2dec9cac3e8f4d5d3c48beb66895267b169ee
IvanekPoul <ivanek.poul@gmail.com>
2013-10-24 11:51:41 +1300

    ...

--- web/try.php
+++ web/try.php
@@ -2,12 +2,12 @@
 
 ?>
   <head>
-   <script type="text/javascript" src="/js/fadeslideshow.js">
+   <script type="text/javascript" src="./js/fadeslideshow.js">
    <script type="text/javascript">
     var mygallery=new fadeSlideShow({
 	    wrapperid: "fadeshow", 
 	    dimensions: [900, 598], 
-	    imagearray: [<?php include("generate.php");?>],
+	    imagearray: [<?php include("./generate.php");?>],
 	    displaymode: {type:'auto', pause:8000, cycles:0, wraparound:false},
 	    persist: false, //remember last viewed slide and recall within same session?
 	    fadeduration: 2500, //transition duration (milliseconds)
