488c41c662f84639ae9347f2f593fd986c1c35e7
IvanekPoul <ivanek.poul@gmail.com>
2013-10-24 12:11:27 +1300

    ,,,

--- web/templates/zahlavi.php
+++ web/templates/zahlavi.php
@@ -22,6 +22,17 @@
     <script type="text/javascript" src="js/fancybox/jquery.mousewheel.pack.js"></script>
     <script type="text/javascript" src="js/fancybox/helpers/jquery.fancybox-thumbs.js"></script>
 
+    <script type="text/javascript">
+    var mygallery=new fadeSlideShow({
+	    wrapperid: "fadeshow1", 
+	    dimensions: [900, 598], 
+	    imagearray: [<?php include("./generate.php");?>],
+	    displaymode: {type:'auto', pause:8000, cycles:0, wraparound:false},
+	    persist: false, //remember last viewed slide and recall within same session?
+	    fadeduration: 2500, //transition duration (milliseconds)
+	    descreveal: "ondemand",
+	    togglerid: ""})
+    </script>
 
     <script type="text/javascript" src="js/bohemia.js"></script>
     <!-- HEADER TEMPLATE END -->
