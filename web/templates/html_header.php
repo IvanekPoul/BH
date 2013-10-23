85f3188b40707522597b694fced1d00a1d5600d9
IvanekPoul <ivanek.poul@gmail.com>
2013-10-24 12:10:21 +1300

    ...

--- web/templates/html_header.php
+++ web/templates/html_header.php
@@ -17,18 +17,7 @@
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
     
-    <script type="text/javascript">
-    var mygallery=new fadeSlideShow({
-	    wrapperid: "fadeshow1", 
-	    dimensions: [900, 600], 
-	    imagearray: [<?php include("/generate.php");?>],
-	    displaymode: {type:'auto', pause:8000, cycles:0, wraparound:false},
-	    persist: false, //remember last viewed slide and recall within same session?
-	    fadeduration: 2500, //transition duration (milliseconds)
-	    descreveal: "ondemand",
-	    togglerid: ""})
-    </script>
-    
+      
     <div id="main">
     
       <div id="top">
